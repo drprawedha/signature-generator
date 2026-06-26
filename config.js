48bd0850e9517fd7d26079b027418e4fc758861f
Rhesa Prawedha <dr.prawedha@gmail.com>
2026-06-26 17:26:44 +0700

    pemindahan config

---
+++ config.js
@@ -0,0 +1,17 @@
+const Signature = {
+
+    name: "RHESA PRAWEDHA",
+
+    title: "IT MANAGER",
+
+    hotel: "MERCURE SOLO",
+
+    address: "Jl. Slamet Riyadi No. 437",
+
+    city: "SOLO - INDONESIA",
+
+    phone: "+62 271 745 11 11",
+
+    email: "RHESA.PRAWEDHA@ACCOR.COM"
+
+};
