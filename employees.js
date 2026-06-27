88f4b33bdfdb8b629635e4682d9c9c3eebe5f962
Rhesa Prawedha <dr.prawedha@gmail.com>
2026-06-27 20:08:23 +0700

    adding employee.js and rename index.html

---
+++ employees.js
@@ -0,0 +1,62 @@
+const Employees = {
+
+    rhesa: {
+        name: "RHESA PRAWEDHA",
+        title: "IT MANAGER",
+        hotel: "MERCURE SOLO",
+        address: "Jl. Slamet Riyadi No. 437",
+        city: "SOLO - INDONESIA",
+        phone: "+62 271 745 11 11",
+        email: "RHESA.PRAWEDHA@ACCOR.COM"
+    },
+
+    dodit: {
+        name: "DODIT HAPSORO",
+        title: "GENERAL MANAGER",
+        hotel: "MERCURE SOLO",
+        address: "Jl. Slamet Riyadi No. 437",
+        city: "SOLO - INDONESIA",
+        phone: "+62 271 745 11 11",
+        email: "DODIT.HAPSORO@ACCOR.COM"
+    },
+
+    diyan: {
+        name: "DIYAN SETIAWATI",
+        title: "ACCOUNTING MANAGER",
+        hotel: "MERCURE SOLO",
+        address: "Jl. Slamet Riyadi No. 437",
+        city: "SOLO - INDONESIA",
+        phone: "+62 271 745 11 11",
+        email: "DIYAN.SETIAWATI@ACCOR.COM"
+    },
+
+    emma: {
+        name: "EMMA WARNIDA",
+        title: "FRONT OFFICE MANAGER",
+        hotel: "MERCURE SOLO",
+        address: "Jl. Slamet Riyadi No. 437",
+        city: "SOLO - INDONESIA",
+        phone: "+62 271 745 11 11",
+        email: "EMMA.WARNIDA@ACCOR.COM"
+    },
+
+    weni: {
+        name: "WENI FITRIATI",
+        title: "DIRECTOR OF SALES AND MARKETING",
+        hotel: "MERCURE SOLO",
+        address: "Jl. Slamet Riyadi No. 437",
+        city: "SOLO - INDONESIA",
+        phone: "+62 271 745 11 11",
+        email: "WENI.FITRIATI@ACCOR.COM"
+    },
+
+    cristanto: {
+        name: "CRISTANTO",
+        title: "ROOM DIVISION MANAGER",
+        hotel: "MERCURE SOLO",
+        address: "Jl. Slamet Riyadi No. 437",
+        city: "SOLO - INDONESIA",
+        phone: "+62 271 745 11 11",
+        email: "CRISTANTO.ANTO@ACCOR.COM"
+    },
+};
